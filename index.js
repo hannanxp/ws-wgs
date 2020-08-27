f1b0310896248887f843c97b72300640daa4016b
Hannan Gigih Prastawa <hannanxp@gmail.com>
2020-08-27 11:13:12 +0700

    loop data show

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ var app = express(); // Create express by calling the prototype in var express
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+function randomStr(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function showData() {
+  console.log("Hello");
+  setTimeout(showData, 3000);
+}
+
+
+
 app.use('/s', express.static(__dirname + '/node_modules/'));
 
 app.get('/', (req, res) => {
@@ -18,4 +35,8 @@ io.on('connection', (socket) => {
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
+  
+  showData();
+  
 });
+
