e4ad28896a7c3df7de6972c16db8c4c1ecf90e2c
Hannan Gigih Prastawa <hannanxp@gmail.com>
2020-08-28 09:40:27 +0700

    websocket

--- ws.js
+++ ws.js
@@ -1,11 +1,42 @@
-const WebSocket = require('ws');
+var express = require('express'); // Get the module
+var app = express(); // Create express by calling the prototype in var express
+var http = require('http').createServer(app);
+var WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 3000 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
+function randomStr(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
-  ws.send('something');
+app.use('/s', express.static(__dirname + '/node_modules/'));
+
+app.get('/', (req, res) => {
+  //res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/ws.html');
 });
+
+
+var myArgs = process.argv.slice(2);
+var port = 80;
+if (myArgs.length > 0) {
+  port = myArgs[0];
+}
+
+http.listen(port, () => {
+  console.log('listening on *:' + port);
+});
+
+var ws = new WebSocket.Server({ port: 3000 });
+ws.on('connection', ws => {
+  setInterval(function(){ 
+    msg = randomStr(10);
+    ws.send('show_panel_2' + ":" + msg);
+  }, 1000);
+  
+})
