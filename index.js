6121a2aba0e7a9ac97b19e780cc7893a5538aac8
Hannan Gigih Prastawa <hannanxp@gmail.com>
2020-08-28 12:44:58 +0700

    add comment

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var app = express(); // Create express by calling the prototype in var express
 var http = require('http').createServer(app);
 var WebSocket = require('ws');
 
-
+/**
+ * Produce random alphanumeric string
+ */
 function randomStr(length) {
   var result           = '';
   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -14,36 +16,49 @@ function randomStr(length) {
   return result;
 }
 
+// static web app
 app.use('/s', express.static(__dirname + '/node_modules/'));
 app.use('/j', express.static(__dirname + '/js/'));
 
+// default routing
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/ws.html');
 });
 
-
+// get command arguments
 var myArgs = process.argv.slice(2);
 var port = 80;
+var wsPort = 3000
 if (myArgs.length > 0) {
   port = myArgs[0];
+  
+  if (myArgs.length > 1) {
+    wsPort = myArgs[1];
+  }
 }
 
+// run webserver
 http.listen(port, () => {
-  console.log('listening on *:' + port);
+  console.log('web server listening on *:' + port);
 });
 
-var ws = new WebSocket.Server({ port: 3000 });
+// run websocket server
+var ws = new WebSocket.Server({ port: wsPort });
 ws.on('connection', ws => {
+  
+  // send message every 2500 ms for panel 1
   setInterval(function(){ 
     msg = randomStr(10);
     ws.send('show_panel_1' + ":" + msg);
   }, 2500);
   
+  // send message every 300 ms for panel 2
   setInterval(function(){ 
     msg = randomStr(10);
     ws.send('show_panel_2' + ":" + msg);
   }, 300);
   
+  // receive incomming message
   ws.on('message', function incoming(message) {
     ws.send(message);
   });
