2b100f35c06924d6e36f2638571b046ad3a4efb4
Hannan Gigih Prastawa <hannanxp@gmail.com>
2020-08-28 09:48:33 +0700

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express'); // Get the module
 var app = express(); // Create express by calling the prototype in var express
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var WebSocket = require('ws');
+
 
 function randomStr(length) {
   var result           = '';
@@ -13,58 +14,11 @@ function randomStr(length) {
   return result;
 }
 
-function showData() {
-  showPanel1();
-  showPanel2();
-  showPanel3();
-  showPanel4();
-}
-
-function showPanel1() {
-  var msg;
-  
-  msg = randomStr(10);
-  io.emit('show_panel_1', msg);
-  
-  setTimeout(showPanel1, 2500);
-}
-
-function showPanel2() {
-  var msg;
-  
-  msg = randomStr(10);
-  io.emit('show_panel_2', msg);
-  
-  setTimeout(showPanel2, 300);
-}
-
-function showPanel3() {
-  var msg;
-  
-  msg = randomStr(10);
-  io.emit('show_panel_3', msg);
-  
-  setTimeout(showPanel3, 1000);
-}
-
-function showPanel4() {
-  var msg;
-  
-  msg = randomStr(10);
-  io.emit('show_panel_4', msg);
-  
-  setTimeout(showPanel4, 3500);
-}
-
 app.use('/s', express.static(__dirname + '/node_modules/'));
 
 app.get('/', (req, res) => {
   //res.sendFile(__dirname + '/index.html');
-  res.sendFile(__dirname + '/panel.html');
-});
-
-io.on('connection', (socket) => {
-  //
+  res.sendFile(__dirname + '/ws.html');
 });
 
 
@@ -76,8 +30,18 @@ if (myArgs.length > 0) {
 
 http.listen(port, () => {
   console.log('listening on *:' + port);
-  
-  showData();
-  
 });
 
+var ws = new WebSocket.Server({ port: 3000 });
+ws.on('connection', ws => {
+  setInterval(function(){ 
+    msg = randomStr(10);
+    ws.send('show_panel_1' + ":" + msg);
+  }, 2500);
+  
+  setInterval(function(){ 
+    msg = randomStr(10);
+    ws.send('show_panel_2' + ":" + msg);
+  }, 300);
+  
+})
